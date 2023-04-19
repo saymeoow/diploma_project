802fb3c1a1ed33fed9fef51ba4ffde0f7ca0ef7c
saymeoow <meowizy94@gmail.com>
2023-04-19 22:34:10 +0300

    .

---
+++ onlinestore/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'store'
+
+urlpatterns = [
+    path('support', views.support,
+         name='support'),
+    path('', views.sneakers_list,
+         name='sneakers_list'),
+    path('<slug:company_slug>/', views.sneakers_list,
+         name='sneakers_by_company'),
+    path('<int:id>/<slug:slug>', views.sneakers_detail,
+         name='sneakers_detail'),
+    path('fake_support', views.fake_support,
+         name='fake_support')
+]
---
+++ register/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.user_login,
+         name='login'),
+    path('logout/', views.logouts,
+         name='logouts'),
+    path('register/', views.register,
+         name='register'),
+    path('edit_profile/', views.edit,
+         name='edit'),
+    path('profile', views.profile,
+         name='profile'),
+    path('profile_personal', views.profile_personal,
+         name='profile_personal'),
+    path('orders_history', views.orders_history,
+         name='orders_history')
+]
